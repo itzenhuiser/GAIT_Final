811975bdea70fe97ec093a28e4736b33dcf17acd
Alex Itzenhuiser <alexitzenhuiser@gmail.com>
2023-12-10 16:44:51 -0600

    pushing changes

--- Interview_assistant.py
+++ Interview_assistant.py
@@ -1,116 +1,129 @@
-# Required libraries
-import requests
-import os
+import gradio as gr
 import openai
-from PIL import Image
-from io import BytesIO
-# API Keys
-openai.api_key = 'your-openai-api-key'
-clipdrop_api_key = 'your-clipdrop-api-key'
-# Initialize global variables
-user_input = "begin"
-game_running = True
-# Messages for the ChatGPT RPG instance
-chatgpt_rpg_messages = [
-{
-"role": "system",
-"content": "I want you to act as the game master of a classic text adventure game set in a lord of the rings like world. Assume the role of the narrator and never break character. Avoid referring to yourself or the outside world. If I need to give you instructions outside the context of the game, I will use curly brackets {like this}. Otherwise, you must maintain the game's setting and narrative. Each location or room should have a detailed description of at least three sentences, and you should always provide me with options to choose from or actions to take. Ensure consistency in the game world, so characters, locations, and items remain as previously described. If I type '{hint}', provide a subtle hint to guide me. Let’s embark on this journey: display the initial setting of the game and await my first command."
-}
-]
-# Messages for the ChatGPT image prompt instance
-chatgpt_imageprompt_messages = [
-{
-"role": "system",
-"content": "As I play a text-based RPG, I will provide you with excerpts,from the game,Your task is to distill these excerpts into A SINGLE, concise text-to-image prompt suitable for DALLE,This prompt should capture the essence of the environment or scene described in the game,Exclude references to my personal interactions, past verbs, or speculations about the story's progression,Imagine you're describing the scene to someone who's observing from a distance, without any personal involvement,Keep track of the context as we proceed, but remember that not every excerpt will introduce a new environment,Please format the prompt following a [PREFIX], [SCENE], [SUFFIX] format where PREFIX defines the image medium, style, perspective; SCENE defines the scene, subject, or context of the image; and SUFFIX defines the overall vibes, adjectives, aesthetic descriptors, lighting, etc,Please provide the prompt as a single plain-text comma separated string with your generated PREFIX, SCENE, and SUFFIX appended together,Provide the prompt without any embellishments like quotes or \"prompt: \","
-}
-]
-# Function to send user input to the ChatGPT instance for RPG responses
-def send_user_input_to_chatgpt_instance_1(user_input):
-# Append user's input to the message list
-    chatgpt_rpg_messages.append(
-    {
-    "role" : "user",
-    "content" : user_input
-    }
-    )
-    # Make an API call to get a response
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=chatgpt_rpg_messages,
-    temperature=1,
-    max_tokens=2048,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    # Extract the message from the response and append to the message list
-    message = response['choices'][0]['message']
-    chatgpt_rpg_messages.append(message)
-    message_content = message['content']
-    return message_content
 
-# Function to send the ChatGPT RPG response to another ChatGPT instance for image prompts
-def send_chatgpt_response_to_chatgpt_instance_2(chatgpt_response):
-# Append the response to the image prompt message list
-    chatgpt_imageprompt_messages.append(
-    {
-    "role" : "user",
-    "content" : chatgpt_response
-    })
-    # Make an API call to get an image prompt
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=chatgpt_imageprompt_messages,
-    temperature=1,
-    max_tokens=1024,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    # Extract the message from the response and append to the message list
-    message = response['choices'][0]['message']
-    chatgpt_imageprompt_messages.append(message)
-    message_content = message['content']
-    return message_content
+# Initialize your OpenAI API key
+openai.api_key = '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'
 
-# Function to generate an image based on the image prompt using the ClipDrop API
-def generate_image(image_prompt):
-    r = requests.post('https://clipdrop-api.co/text-to-image/v1',
-    files = {
-    'prompt': (None, image_prompt, 'text/plain')
-    },
-    headers = { 'x-api-key': clipdrop_api_key}
+# Initialize conversation lists
+bob_system_prompt = {
+    "role": "system",
+    "content": "You are Bob, an expert interviewer in all fields. You will be holding a mock interview with a user. Your sole job is to ask popular interview questions to a user. Your questions should not be longer than three sentences long. There will be a separate assistant, Dan, who will be grading the user's responses to your questions. Because of this, you do not need to give any feedback on user's responses. You will simply continue to ask questions to the user until they say stop. You will BEGIN asking interview questions when the user says 'start'. The interview will END when the user says anything along the lines of 'im done'. To be extra clear, whenever the user says 'start', YOU need to start asking interview quesitons."
+}
+dan_system_prompt = {
+    "role": "system",
+    "content": "Dan is a communications specialist, with an expert knowledgebase on interviews. Dan will oversee mock interviews between a user and a different assistant, Bob, who is asking the interview questions. Dan's sole purpose is to grade the users' responses to Bob's interview questions. The assigned grades can be anything in-between an F and an A+. Dans response should only consist of a grade for the users answer to the question, a concise bullet point list of what the user said that was good, and a concise bullet point list of areas that the user can work on in their response. Do not be afraid to give a poor grade to what you might consider a poor response, you will not hurt anyones feelings. Giving poor grades is good feedback for the user so they know what they need to improve upon. For a baseline, anything average, that does not go above and beyond, should be graded as a C. Additionally, when you recieve the input 'SUMMARIZE', you are to re read over the full interview, and provide a grade based on all of the answers that the user gave. Your response should start with 'Full Interview Summary' and you should then provide a grade in the same format that you do for individual questions."
+}
+
+# Initialize conversation lists
+chatgpt_bob_messages = []
+chatgpt_dan_messages = []
+
+# Function to send user input to Bob and get a response
+def send_to_bob(user_input):
+    messages = [bob_system_prompt] + chatgpt_bob_messages
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
+        temperature=1,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    if (r.ok):
-    # r.content contains the bytes of the returned image
-        image_bytes = r.content
-        return image_bytes
-    else:
-        r.raise_for_status()
-# Function to display the generated image
-def display_image(image_bytes):
-    # Convert the bytes to an image format
-    byte_stream = BytesIO(image_bytes)
-    image = Image.open(byte_stream)
-    # Display the image using the default image viewer
-    image.show()
-# Main loop for the game
-while game_running:
-# Get a game-related response from the ChatGPT RPG instance
-    chatgpt_response = send_user_input_to_chatgpt_instance_1(user_input)
-    print(chatgpt_response)
-    # Get an image prompt based on the game response
-    image_prompt = send_chatgpt_response_to_chatgpt_instance_2(chatgpt_response)
-    print(image_prompt)
-    # Generate an image based on the image prompt
-    generated_image_bytes = generate_image(image_prompt)
-    # Display the generated image
-    display_image(generated_image_bytes)
-    # Get the next user input
+    message = response['choices'][0]['message']
+    chatgpt_bob_messages.append(message)
+    return message['content']
+
+# Function to send Bob's response to Dan and get feedback
+def send_to_dan(bob_response):
+    messages = [dan_system_prompt] + chatgpt_dan_messages
+    messages.append({"role": "user", "content": bob_response})
+    response = openai.ChatCompletion.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    message = response['choices'][0]['message']
+    chatgpt_dan_messages.append(message)
+    return message['content']
+
+def format_conversation(user_input):
+    formatted_conversation = ""
+
+    # Display the last message from Bob (if any)
+    if chatgpt_bob_messages:
+        last_bob_message = chatgpt_bob_messages[-1]['content']
+        formatted_conversation += "Bob: " + last_bob_message + "\n"
+
+    # Display the user's input
+    if user_input:
+        formatted_conversation += "User: " + user_input + "\n"
+
+    # Display the last message from Dan (if any)
+    if chatgpt_dan_messages:
+        last_dan_message = chatgpt_dan_messages[-1]['content']
+        formatted_conversation += "Dan: " + last_dan_message + "\n"
+
+    return formatted_conversation
+
+
+
+
+def handle_interview(user_input, is_user_response, ask_new_question):
+    global chatgpt_bob_messages, chatgpt_dan_messages
+
+    if user_input.lower() == "start":
+        chatgpt_bob_messages.clear()
+        chatgpt_dan_messages.clear()
+        send_to_bob("Please start the interview.")
+        is_user_response = True
+        ask_new_question = False
+    elif is_user_response:
+        send_to_dan(user_input)
+        is_user_response = False
+        ask_new_question = True
+    elif ask_new_question:
+        send_to_bob(user_input)
+        is_user_response = True
+        ask_new_question = False
+
+    conversation = format_conversation(user_input)
+    return conversation, is_user_response, ask_new_question
+
+
+
+
+
+
+# Create Gradio Interface
+# iface = gr.Interface(
+#     fn=handle_interview,
+#     inputs=gr.Textbox(lines=2, placeholder="Type here..."),
+#     outputs=gr.Textbox(label="Conversation"),
+# )
+
+
+# # Launch the application
+# iface.launch()
+
+def main():
+    print("Type 'start' to begin the interview, or 'exit' to quit:")
     user_input = input("User: ")
-    # End the game if the user types "exit"
-    if user_input.lower() == "exit":
-        game_running = False
+    is_user_response = False
+    ask_new_question = False
+    while user_input.lower() != 'exit':
+        conversation, is_user_response, ask_new_question = handle_interview(user_input, is_user_response, ask_new_question)
+        print(conversation)
+        user_input = input("User: ")
 
-# Game loop ends
-print("Game over. Thanks for playing!")
+if __name__ == "__main__":
+    main()
+
+
+
+
